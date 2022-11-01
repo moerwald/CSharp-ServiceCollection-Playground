217717ff2851dac91fc944959d708b326738d275
moerwald <andreas.mewald@gmail.com>
2022-11-01 18:13:01 +0100

    Added project

---
+++ src/ServiceCollectionPlayground/Program.cs
@@ -0,0 +1,45 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+using Microsoft.Extensions.DependencyInjection;
+
+public static class Program {
+    public static void Main (string [] args)
+    {
+        var services = new ServiceCollection();
+        services.AddSingleton<IMyLog,MyConsoleLogger>();
+        //services.AddSingleton<IFoo,FooForDemo>(provider => new FooForDemo(provider));
+        services.AddSingleton<IFoo,FooForDemo>();
+
+        var provider = services.BuildServiceProvider();
+
+        IFoo demoFoo = provider.GetService<IFoo>()!;
+
+        demoFoo.Bar();
+    }
+}
+
+interface IMyLog {
+    void Information(string messageToLog);
+}
+
+class MyConsoleLogger : IMyLog
+{
+    public void Information(string messageToLog) => System.Console.WriteLine(messageToLog);
+}
+
+interface IFoo
+{
+    void Bar();
+}
+
+class FooForDemo : IFoo
+{
+    private IServiceProvider _provider;
+
+    public FooForDemo(IServiceProvider provider) => _provider = provider;
+
+    public void Bar()
+    {
+        _provider.GetService<IMyLog>()?.Information(nameof(Bar));
+    }
+}
